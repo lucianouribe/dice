cced222c9dd06a8d906ccaf67661db6cb331c6cc
Luciano Uribe <lucianouribe@gmail.com>
2016-11-06 14:57:30 -0700

    Add page added

--- server.rb
+++ server.rb
@@ -12,6 +12,19 @@ post '/' do
   erb :index
 end
 
+# add page
+get '/add' do
+  erb :add
+end
+
+post '/add' do
+  $sum_of_all_arrays = []
+  @user_list = []
+  frase = params[:new_input]
+  @frase = frase
+  erb :add
+end
+
 # Not found page
 not_found do
   status 404
